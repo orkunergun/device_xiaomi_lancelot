d76826fa7033f32ce3fe1fbdb2b393eb11f44efa
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2023-06-21 11:56:47 +0000

    lancelot: Build required Thermal blobs from source
    Signed-off-by: Orkun Ergün <104510596+orkunergun@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -346,6 +346,12 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.hardware.sensors@2.1.vendor
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
