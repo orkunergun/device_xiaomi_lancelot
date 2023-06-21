7f4d9fe474ec77a1f009b075203e75d33a642e57
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2023-06-21 11:37:19 +0000

    lancelot: Build required Neural Networks blobs from source
    Signed-off-by: Orkun Ergün <104510596+orkunergun@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -195,6 +195,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpcap.vendor
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0.vendor \
+    android.hardware.neuralnetworks@1.1.vendor \
+    android.hardware.neuralnetworks@1.2.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
