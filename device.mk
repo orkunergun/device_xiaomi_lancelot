b830844e693f39f67b9f44b3d216d8de34eb9361
SamarV-121 <samarvispute121@pm.me>
2023-06-22 17:56:06 +0000

    lancelot: audio: Build and rename audio service HAL
    Signed-off-by: Orkun Ergün <104510596+orkunergun@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service.mediatek
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@7.0.vendor \
