27d8a52185134b12018bf3a00ddcd54f3398edb3
Matsvei Niaverau <matvej2002@gmail.com>
2023-06-24 11:48:02 +0000

    lancelot: Bring back IMS bootjars
    Signed-off-by: Orkun Ergün <104510596+orkunergun@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -153,6 +153,19 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libunwindstack.vendor
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
